aae3c337c62599c3ed11255b9d15a0f4d644fd6e
pandykad <pratikkakade542@gmail.com>
2024-08-20 12:43:41 -0500

    removing node modules

--- node_modules/react-quizlet-flashcard/dist/components/Flashcard/Flashcard.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-import FlashcardProps from "../../interfaces/IFlashcard";
-import "./Flashcard.scss";
-declare function Flashcard({ frontHTML, frontCardStyle, frontContentStyle, backHTML, backCardStyle, backContentStyle, className, style, height, borderRadius, width, onCardFlip, manualFlipRef, }: FlashcardProps): JSX.Element;
-export default Flashcard;
